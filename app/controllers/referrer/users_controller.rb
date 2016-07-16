957f1b520aeb0f37a80de4897c819aecdeae0646
salkar <sokolov.sergey.a@gmail.com>
2016-07-16 13:11:23 -0400

    fix and finish js file

--- app/controllers/referrer/users_controller.rb
+++ app/controllers/referrer/users_controller.rb
@@ -6,11 +6,5 @@ module Referrer
       @user = User.create!
       render json: {id: @user.id, token: @user.token}
     end
-
-    private
-
-    def user_params
-      params.require(:user).permit
-    end
   end
 end
